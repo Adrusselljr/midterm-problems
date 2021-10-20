76e36925970f276dff95861f4edecc29ff7ae37f
Aaron Russell Jr <adrusselljr@Aarons-MBP.lan>
2021-10-20 13:00:17 -0400

    modified js

--- main.js
+++ main.js
@@ -1,43 +1,198 @@
-function isEvenlyDivisible() {
+function isEvenlyDivisible(num1, num2) {
+  if(num1 % num2 === 0) {
+    return true
+  }
+  else {
+    return false
+  }
 }
 
-function halfSquare() {
+function halfSquare(num) {
+  return (num * num) / 2
 }
 
-function isLong() {
+function isLong(str) {
+  let count = 0
+  for(let i = 0; i < str.length; i++) {
+    if(str.length >= 15) {
+      return true
+    }
+    else {
+      return false
+    }
+  }
 }
 
-function exclaim() {
+function exclaim(str) {
+  let count = 0
+  for(let i = 0; i < str.length; i++) {
+      if(str[i] === '!') {
+          count++
+      }
+  }
+  if(str[str.length - 1] !== '!') {
+      return `${str}!`
+  }
+  if(count === 1) {
+      return str
+  }
+  if(count > 1) {
+    return str.slice(0, -count + 1)
+  }
 }
 
-function countWords() {
+function countWords(str) {
+  let count = 0
+  for(let i = 0; i < str.length; i++) {
+      if(str[i] === " ") {
+          count++
+      }
+  }
+  return count += 1
 }
 
-function containsDigit() {
+function containsDigit(str) {
+  let count = 0
+  const digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+  for(let i = 0; i < str.length; i++) {
+    if(digits.includes(str[i]))
+    count++
+  }
+  if(count >= 1) {
+    return true
+  }
+  else {
+    return false
+  }
 }
 
-function containsLowerCase() {
+function containsLowerCase(str) {
+  let count = 0
+  const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v" , "w", "x", "y", "z"]
+  for(let i = 0; i < str.length; i++) {
+    if(lowerCase.includes(str[i]))
+    count++
+  }
+  if(count >= 1) {
+    return true
+  }
+  else {
+    return false
+  }
 }
 
-function containsUpperCase() {
+function containsUpperCase(str) {
+  let count = 0
+  const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V" , "W", "X", "Y", "Z"]
+  for(let i = 0; i < str.length; i++) {
+    if(upperCase.includes(str[i]))
+    count++
+  }
+  if(count >= 1) {
+    return true
+  }
+  else {
+    return false
+  }
 }
 
-function containsNonAlphanumeric() {
+function containsNonAlphanumeric(str) {
+  let count = 0
+  const specialChar = ["!", "'", '"', "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", ";", ":", ",", "<", ".", ">", "/", "?", "`", "~", " "]
+  for(let i = 0; i < str.length; i++) {
+    if(specialChar.includes(str[i]))
+    count++
+  }
+  if(count >= 1) {
+    return true
+  }
+  else {
+    return false
+  }
 }
 
-function containsSpace() {
+function containsSpace(str) {
+  let count = 0
+  for(let i = 0; i < str.length; i++) {
+    if(str[i] === " "){
+      count++
+    }
+  }
+  if(count >= 1) {
+    return true
+  }
+  else {
+    return false
+  }
 }
 
-function digits() {
+function digits(num) {
+  let arr = ("" + num).split('').map(Number)
+  for(let i = 0; i < arr.length; i++) {
+      if(isNaN(arr[i])) {
+          arr.splice(i, 1)
+      }
+  }
+  return arr
 }
 
-function truncate() {
+function truncate(str) {
+  let output = ""
+  for(let i = 0; i < 8; i++) {
+      output += str[i]
+  }
+  if(str.length < 15) {
+      return str
+  }
+  else {
+      return `${output}...`
+  }
 }
 
-function isValidPassword() {
+function isValidPassword(str) {
+  let count1 = 0
+  const digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+  let count2 = 0
+  const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v" , "w", "x", "y", "z"]
+  let count3 = 0
+  const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V" , "W", "X", "Y", "Z"]
+  let count4 = 0
+  const specialChar = ["!", "'", '"', "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", ";", ":", ",", "<", ".", ">", "/", "?", "`", "~", " "]
+  let count5 = 0
+  for(let i = 0; i < str.length; i++) {
+    if(digits.includes(str[i])) {
+      count1++
+    }
+    if(lowerCase.includes(str[i])) {
+      count2++
+    }
+    if(upperCase.includes(str[i])) {
+      count3++
+    }
+    if(specialChar.includes(str[i])) {
+      count4++
+    }
+    if(str[i] === " ") {
+      count5++
+    }
+  }
+  if(count1 >= 1 && count2 >= 1 && count3 >= 1 && count4 >= 1 && count5 === 0) {
+    return true
+  }
+  else {
+    return false
+  }
 }
 
-function onlyPunchy() {
+function onlyPunchy(arr) {
+  let outputArr = []
+  for(let i = 0; i < arr.length; i++) {
+    const word = exclaim(arr[i])
+    if(word.length < 15) {
+      outputArr.push(exclaim(arr[i]))
+    }
+  }
+  return outputArr
 }
 
 
